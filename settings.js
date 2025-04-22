6e59f213b5e1f79ab024ffa1557eb7487a6e65a2
Jarvismd567 <ziabot96@gmail.com>
2025-04-22 18:11:10 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,15 +3,15 @@ const chalk = require('chalk')
 
 
 //contact details
-global.ownernomer = "2348109263390"
+global.ownernomer = "918891131521"
 global.ownername = "ElitePro"
 global.ytname = "YT: @ElitePro-Empire"
 global.socialm = "GitHub: Elite-MD"
 global.location = "Nigeria, Port Harcourt"
 
 
-global.ownernumber = '2348109263390'  //creator number
-global.ownername = 'ElitePro' //owner name
+global.ownernumber = '918891131521'  //creator number
+global.ownername = 'BROKEN SPIRIT' //owner name
 global.botname = 'ELITE-PRO-V1' //name of the bot
 
 
